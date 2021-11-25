b054f9d5b437a0cbded9b0532cdf00f030a4b504
John Garnham <john.e.garnham@gmail.com>
2021-11-25 01:42:12 -0700

    ?help uses botPrefix

--- priv_cmds/help.js
+++ priv_cmds/help.js
@@ -1,3 +1,7 @@
+// Shows the help menu (any way to auto-generate this?)
+
+const CFG = require('../config.json')
+
 module.exports = {
   command: 'help',
   execute (client, event, env) {
@@ -5,17 +9,17 @@ module.exports = {
       recipient_id: env.senderId,
       text: `Commands:\n\n
 You can use this commands on DM channel of bot.
-?help : Shows commands
-?deposit : Gives deposit wallet address and memo
-?withdraw (rec.) (amo.) <token> : Withdraws assets to recipient
-?balance(bal) <token> : Shows balance
-?tip (@user) (amount) <token> : Tip users semi-privately
-?statistics(stats) <token> : Shows stats
+${CFG.botPrefix}help : Shows commands
+${CFG.botPrefix}deposit : Gives deposit wallet address and memo
+${CFG.botPrefix}withdraw (rec.) (amo.) <token> : Withdraws assets to recipient
+${CFG.botPrefix}balance(bal) <token> : Shows balance
+${CFG.botPrefix}tip (@user) (amount) <token> : Tip users semi-privately
+${CFG.botPrefix}statistics(stats) <token> : Shows stats
 \nReply(tweet) Commands:\n\n
 You can use this commands by replying tweets with mentioning bot.
-?tip (amount) <token>
-\nExample DM command: ?tip @TipbotVite 1 VITE
-Example Reply command: @TipbotVite ?tip 1 VITE`
+${CFG.botPrefix}tip (amount) <token>
+\nExample DM command: ${CFG.botPrefix}tip @TipbotVite 1 VITE
+Example Reply command: @TipbotVite ${CFG.botPrefix}tip 1 VITE`
     })
   }
 }
